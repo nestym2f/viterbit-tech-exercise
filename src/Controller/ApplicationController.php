95f2ae1b8fe0590b3f15f728116a441bc0905835
Ernesto Figueredo <ernesto@jewelersconnect.com>
2024-03-01 12:35:27 -0500

    Create ApplicationController

---
+++ src/Controller/ApplicationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use App\Document\Application;
+use App\Form\Type\ApplicationType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApplicationController extends AbstractController
+{
+    
+}
