9c47d2111d182e699953e7250e398675f31fc21f
Ernesto Figueredo <ernesto@jewelersconnect.com>
2024-03-01 12:57:14 -0500

    Add applications route to ApplicationController

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -3,13 +3,28 @@
 namespace App\Controller;
 
 use App\Document\Application;
-use App\Form\Type\ApplicationType;
+use App\Service\ApplicationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ApplicationController extends AbstractController
-{
-    
+{    
+    private $applicationService;
+
+    public function __construct(ApplicationService $applicationService)
+    {        
+        $this->applicationService = $applicationService;
+    }
+
+    #[Route('/applications', name: 'applications')]
+    public function jobsListAction(): Response
+    {
+        //retrieve all applications
+        $applications = $this->applicationService->getAllApplications();
+        return $this->render('applications/applications.html.twig', [
+            'applications' => $applications,
+        ]);
+    }
 }
